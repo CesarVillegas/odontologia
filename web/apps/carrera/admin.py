ca0bc36b92e4286214a5e042a3a4dd2de46a84d0
cesar_campus_digital <cesar.villegas.cortes@gmail.com>
2018-01-22 15:38:37 -0300

    Se suben cambios en admision, docentes y contacto

--- web/apps/carrera/admin.py
+++ web/apps/carrera/admin.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from .models import Admision
-from .models import Contenido
+from .models import Admision, Contenido, Docentes
 
 
 # Register your models here.
 class admisionAdmin(admin.ModelAdmin):
     list_display = ('anio','puntaje_minimo','puntaje_maximo','puntaje_ponderado', 'vacantes')
 
-admin.site.register(Admision, admisionAdmin)
-
-
 class ContenidoAdmin(admin.ModelAdmin):
     list_display = (['seccion'])
 
+class DocenteAdmin(admin.ModelAdmin):
+    list_display = ('nombre_completo', 'image_foto', 'cargo')
+
+
+admin.site.register(Admision, admisionAdmin)
+admin.site.register(Docentes, DocenteAdmin)
 admin.site.register(Contenido, ContenidoAdmin)
--- web/apps/carrera/models.py
+++ web/apps/carrera/models.py
@@ -7,6 +7,24 @@ from django.core.exceptions import ValidationError
 from django.core.files.images import get_image_dimensions
 from ckeditor_uploader.fields import RichTextUploadingField
 
+
+TITULO = (
+    ('candidato_doctor', 'C.D.'),
+    ('profesor', 'Profesor'),
+    ('magister', 'M.G'),
+)
+
+def validate_image(fieldfile_obj):
+    filesize = fieldfile_obj.file.size
+    megabyte_limit = 1.0
+    if filesize > megabyte_limit*1024*1024:
+        raise ValidationError("El tamano maximo permitido es de %sMB" % str(megabyte_limit))
+    w, h = get_image_dimensions(fieldfile_obj)
+    if w != 270 and h != 270:
+        raise ValidationError("Las dimensiones de la foto son de %ix%i, y estas deben ser de 270x270 pixeles" %(h,w))
+
+
+
 # Create your models here.
 class Admision(models.Model):
     anio = models.PositiveSmallIntegerField(verbose_name="año")
@@ -31,9 +49,40 @@ class Contactos(models.Model):
     email = models.EmailField(max_length=254, blank=False, null=False)
     celular = models.CharField(max_length=8, blank=False, null=False)
     mensaje = RichTextField(max_length=500, blank=True, null=True, config_name='awesome_ckeditor')
+    fecha_contacto = models.DateTimeField()
 
+    class Meta:
+        ordering = ["fecha_contacto"]
 
 # Clase para definición de contenido estandar
 class Contenido(models.Model):
 	seccion = models.CharField(max_length=100, blank=False, null=False)
-	texto = RichTextUploadingField(max_length=5000, blank=True, null=True, config_name='awesome_ckeditor')
+	texto = RichTextUploadingField(max_length=10000, blank=True, null=True, config_name='awesome_ckeditor')
+
+
+class Docentes(models.Model):
+    nombres = models.CharField(max_length=100, blank=False, null=False)
+    apellido_paterno = models.CharField(max_length=100, blank=False, null=False)
+    apellido_materno = models.CharField(max_length=100, blank=True, null=True)
+    cargo = models.CharField(max_length=100, blank=False, null=False)
+    titulo = models.CharField(max_length=20, choices=TITULO)
+
+    foto = models.ImageField(upload_to='carrera/docente/', height_field=None, width_field=None, max_length=100, validators=[validate_image], help_text='Tamano maximo de la imagen es 1Mb, sus dimensiones deben ser de 270x270 pixeles')
+    nivel = models.IntegerField(default=1)
+
+    class Meta:
+        ordering = ["nivel"]
+
+    def __str__(self): # __unicode__ en Python 2
+        return '%s %s %s' % (self.nombres.encode('utf8'), self.apellido_paterno.encode('utf8'), self.apellido_materno.encode('utf8'))
+
+    def nombre_completo(self):
+        return '%s %s %s' % (self.nombres.encode('utf8'), self.apellido_paterno.encode('utf8'), self.apellido_materno.encode('utf8'))
+
+    def image_foto(self):
+          if self.foto:
+              return u'<img width="270px" height="auto" src="%s" />' % self.foto.url
+          else:
+              return '(Sin imagen)'
+    image_foto.short_description = 'Imagen'
+    image_foto.allow_tags = True
