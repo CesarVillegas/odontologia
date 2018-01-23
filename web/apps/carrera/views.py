9b0874d187365a55426c90632a3ef1ad65e94cfe
Juan Galleguillos <juangalleguillosc@gmail.com>
2018-01-23 15:09:54 -0300

    Se agregan las paginas de error a la vista de carrera

--- web/apps/carrera/views.py
+++ web/apps/carrera/views.py
@@ -88,3 +88,9 @@ def organigrama(request):
 def docentes(request):
     docentes = Docentes.objects.all()
     return render(request, 'carrera/lista_docentes.html', {'docentes': docentes})
+
+def handler404(request):
+    return render(request, '404.html', status=404)
+
+def handler500(request):
+    return render(request, '500.html', status=500)
