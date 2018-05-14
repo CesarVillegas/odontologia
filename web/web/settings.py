8064fffd42b44178b05686235fbb8194d3b7f8f8
Juan Galleguillos <juangalleguillosc@gmail.com>
2018-05-14 16:14:29 -0400

    Se define entorno de desarrollo con docker

--- web/web/settings.py
+++ web/web/settings.py
@@ -80,16 +80,27 @@ WSGI_APPLICATION = 'web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'webodonto',
-        'USER': 'userodonto',
-        'PASSWORD': 'odontoq1w2e3',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+if DEBUG:
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+
+else:
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'webodonto',
+            'USER': 'userodonto',
+            'PASSWORD': 'odontoq1w2e3',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+        }
+    }
 
 
 # Password validation
