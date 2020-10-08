50c9639f07218486dcfbf68c1c5f7b92205047ab
Juan Galleguillos <juangalleguillosc@gmail.com>
2020-10-08 13:33:28 -0300

    Se modifica docker compose y setting con sqlite para enterno de desarrollo

--- web/web/settings.py
+++ web/web/settings.py
@@ -85,12 +85,8 @@ if DEBUG:
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'webodonto',
-            'USER': 'userodonto',
-            'PASSWORD': 'odontoq1w2e3',
-            'HOST': 'db',
-            'PORT': '3306',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
