052da5b97d522a238ac3056ab14193e3d6d77361
Juan Galleguillos <juangalleguillosc@gmail.com>
2020-10-28 16:20:23 -0300

    se agrega al modelo de carrera la validacion de la imagen de la actividad

--- web/apps/carrera/models.py
+++ web/apps/carrera/models.py
@@ -33,6 +33,16 @@ def validate_image(fieldfile_obj):
         raise ValidationError("Las dimensiones de la foto son de %ix%i, y estas deben ser de 270x270 pixeles" %(h,w))
 
 
+def validate_image_actividad(fieldfile_obj):
+    filesize = fieldfile_obj.file.size
+    megabyte_limit = 1.0
+    if filesize > megabyte_limit*1024*1024:
+        raise ValidationError("El tamano maximo permitido es de %sMB" % str(megabyte_limit))
+    w, h = get_image_dimensions(fieldfile_obj)
+    if w != 420 and h != 420:
+        raise ValidationError("Las dimensiones de la imagen son de %ix%i, y estas deben ser de 420x420 pixeles" %(h,w))
+
+
 # Create your models here.
 class Admision(models.Model):
     anio = models.PositiveSmallIntegerField(verbose_name="año")
@@ -101,3 +111,25 @@ class Docentes(models.Model):
               return '(Sin imagen)'
     image_foto.short_description = 'Imagen'
     image_foto.allow_tags = True
+
+
+# Clase para las actividades
+class Actividad(models.Model):
+    titulo = models.CharField(max_length=150, blank=False, null=False)
+    descripcion = RichTextUploadingField(max_length=35000, blank=True, null=True, config_name='awesome_ckeditor')
+    inicio = models.DateTimeField(blank=True, null=True)
+    cierre = models.DateTimeField(blank=True, null=True)
+    imagen = models.ImageField(upload_to='calendario/', height_field=None, width_field=None, max_length=100, blank=False, validators=[validate_image_actividad], help_text='Tamano maximo de la imagen es 1Mb, sus dimensiones deben ser de 420x420 pixeles')
+    mostrar = models.BooleanField(default=True, help_text="Visualizar actividad.")
+
+    class Meta:
+        verbose_name_plural = "Actividades"
+
+    def image_preview(self):
+        if self.imagen:
+            return u'<img width="100px" height="auto" src="%s" />' % self.imagen.url
+        else:
+            return '(Sin imagen)'
+    image_preview.short_description = 'Imagen'
+    image_preview.allow_tags = True
+
