edc4d20651ccb1306f3190880002128c93f2503d
Abel Aracena <aracena.abel@gmail.com>
2020-10-30 12:48:07 -0300

    Se hacen los cambios a la sección de investigación de odontología.

--- web/apps/investigacion/views.py
+++ web/apps/investigacion/views.py
@@ -13,3 +13,18 @@ def index(request):
     except EmptyPage:
         documentos = paginator.page(paginator.num_pages)
     return render(request, 'investigacion/index.html',{'documentos':documentos})
+
+def documentos(request):
+    docs = Documento.objects.filter(mostrar=True, tipo_documento='I').order_by('titulo').order_by('prioridad')
+    paginator = Paginator(docs, 6)
+    page = request.GET.get('page')
+    try:
+        documentos = paginator.page(page)
+    except PageNotAnInteger:
+        documentos = paginator.page(1)
+    except EmptyPage:
+        documentos = paginator.page(paginator.num_pages)
+    return render(request, 'investigacion/documentos.html',{'documentos':documentos})
+
+def cienciometria(request):
+    return render(request, 'investigacion/cienciometria.html')
