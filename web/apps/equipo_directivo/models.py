defe971cd7352688f9501d5c3ecc49200ff1ef41
Abel Aracena <aracena.abel@gmail.com>
2020-11-06 16:27:03 -0300

    Se agregan las apps para poder subir los coordanciones por área y equipo directivo.

---
+++ web/apps/equipo_directivo/models.py
@@ -0,0 +1,47 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.files.images import get_image_dimensions
+
+# Create your models here.
+def validate_image_docente(fieldfile_obj):
+    filesize = fieldfile_obj.file.size
+    megabyte_limit = 1.0
+    if filesize > megabyte_limit*1024*1024:
+        raise ValidationError("El tamano maximo permitido es de %sMB" % str(megabyte_limit))
+    w, h = get_image_dimensions(fieldfile_obj)
+    if w != 270 and h != 270:
+        raise ValidationError("Las dimensiones de la foto son de %ix%i, y estas deben ser de 270x270 pixeles" %(h,w))
+
+# Clase para los directivos de la carrera
+class Directivo(models.Model):
+    TIPO_DIRECTIVO = (
+        ('D','Dirección'),
+        ('A','Administración'),
+        ('C','Coordinación'),
+    )
+    nombres = models.CharField(max_length=100, blank=False, null=False)
+    apellido_paterno = models.CharField(max_length=100, blank=False, null=False)
+    apellido_materno = models.CharField(max_length=100, blank=False, null=False)
+    cargo = models.CharField(max_length=100, blank=False, null=False)
+    tipo_cargo = models.CharField(max_length=1, choices=TIPO_DIRECTIVO, blank=False, null=False, default='G')
+    foto = models.ImageField(upload_to='equipo_directivo/directivos/', height_field=None, width_field=None, max_length=100, validators=[validate_image_docente], help_text='Tamano maximo de la imagen es 1Mb, sus dimensiones deben ser de 270x270 pixeles')
+    nivel = models.IntegerField(default=1)
+
+    class Meta:
+        verbose_name_plural = "Directivos"
+        ordering = ["nivel"]
+
+    def __unicode__(self): # __str__ en Python 3
+        return '%s %s %s' % (self.nombres, self.apellido_paterno, self.apellido_materno)
+
+    def nombre_completo(self):
+        return '%s %s %s' % (self.nombres, self.apellido_paterno, self.apellido_materno)
+
+    def image_foto(self):
+          if self.foto:
+              return u'<img width="270px" height="auto" src="%s" />' % self.foto.url
+          else:
+              return '(Sin imagen)'
+    image_foto.short_description = 'Imagen'
+    image_foto.allow_tags = True
